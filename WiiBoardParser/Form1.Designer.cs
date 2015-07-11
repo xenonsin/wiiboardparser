de71c77e095f025217cb3a4513145868db93af5d
xenonsin <kenoiwasp@gmail.com>
2015-07-11 09:22:57 -0700

    ini

---
+++ WiiBoardParser/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WiiBoardParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_RawWeight = new System.Windows.Forms.GroupBox();
+            this.label_rwBR = new System.Windows.Forms.Label();
+            this.label_rwBL = new System.Windows.Forms.Label();
+            this.label_rwTR = new System.Windows.Forms.Label();
+            this.label_rwTL = new System.Windows.Forms.Label();
+            this.label_rwWT = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.groupBox_RawWeight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_RawWeight
+            // 
+            this.groupBox_RawWeight.Controls.Add(this.label_rwBR);
+            this.groupBox_RawWeight.Controls.Add(this.label_rwBL);
+            this.groupBox_RawWeight.Controls.Add(this.label_rwTR);
+            this.groupBox_RawWeight.Controls.Add(this.label_rwTL);
+            this.groupBox_RawWeight.Controls.Add(this.label_rwWT);
+            this.groupBox_RawWeight.Location = new System.Drawing.Point(62, 47);
+            this.groupBox_RawWeight.Name = "groupBox_RawWeight";
+            this.groupBox_RawWeight.Size = new System.Drawing.Size(150, 139);
+            this.groupBox_RawWeight.TabIndex = 4;
+            this.groupBox_RawWeight.TabStop = false;
+            this.groupBox_RawWeight.Text = "Raw Weight";
+            // 
+            // label_rwBR
+            // 
+            this.label_rwBR.AutoSize = true;
+            this.label_rwBR.Location = new System.Drawing.Point(101, 76);
+            this.label_rwBR.Name = "label_rwBR";
+            this.label_rwBR.Size = new System.Drawing.Size(22, 13);
+            this.label_rwBR.TabIndex = 0;
+            this.label_rwBR.Text = "BR";
+            // 
+            // label_rwBL
+            // 
+            this.label_rwBL.AutoSize = true;
+            this.label_rwBL.Location = new System.Drawing.Point(25, 76);
+            this.label_rwBL.Name = "label_rwBL";
+            this.label_rwBL.Size = new System.Drawing.Size(20, 13);
+            this.label_rwBL.TabIndex = 0;
+            this.label_rwBL.Text = "BL";
+            // 
+            // label_rwTR
+            // 
+            this.label_rwTR.AutoSize = true;
+            this.label_rwTR.Location = new System.Drawing.Point(101, 32);
+            this.label_rwTR.Name = "label_rwTR";
+            this.label_rwTR.Size = new System.Drawing.Size(22, 13);
+            this.label_rwTR.TabIndex = 0;
+            this.label_rwTR.Text = "TR";
+            // 
+            // label_rwTL
+            // 
+            this.label_rwTL.AutoSize = true;
+            this.label_rwTL.Location = new System.Drawing.Point(25, 32);
+            this.label_rwTL.Name = "label_rwTL";
+            this.label_rwTL.Size = new System.Drawing.Size(20, 13);
+            this.label_rwTL.TabIndex = 0;
+            this.label_rwTL.Text = "TL";
+            // 
+            // label_rwWT
+            // 
+            this.label_rwWT.AutoSize = true;
+            this.label_rwWT.Location = new System.Drawing.Point(63, 113);
+            this.label_rwWT.Name = "label_rwWT";
+            this.label_rwWT.Size = new System.Drawing.Size(25, 13);
+            this.label_rwWT.TabIndex = 0;
+            this.label_rwWT.Text = "WT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Keno\'s Balance Board to Unity Parser";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(49, 202);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(183, 48);
+            this.button_Connect.TabIndex = 6;
+            this.button_Connect.Text = "Connect to Wii balance board";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox_RawWeight);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox_RawWeight.ResumeLayout(false);
+            this.groupBox_RawWeight.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_RawWeight;
+        private System.Windows.Forms.Label label_rwBR;
+        private System.Windows.Forms.Label label_rwBL;
+        private System.Windows.Forms.Label label_rwTR;
+        private System.Windows.Forms.Label label_rwTL;
+        private System.Windows.Forms.Label label_rwWT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Connect;
+    }
+}
+
---
+++ WiiBoardParser/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using WiimoteLib;
+using System.IO;
+
+namespace WiiBoardParser
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer infoUpdateTimer = new System.Timers.Timer() { Interval = 50, Enabled = false };
+        //System.Timers.Timer joyResetTimer = new System.Timers.Timer() { Interval = 240000, Enabled = false };
+
+        Wiimote wiiDevice = new Wiimote();
+        //string path = @"D:\Projects\WiiBoardParser\WiiBoardParser"; //Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string path = @"D:\Projects\HackCU\HackCU\Assets\Wii Fit\WiiWeight.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            // Setup a timer which controls the rate at which updates are processed.
+
+            infoUpdateTimer.Elapsed += new ElapsedEventHandler(infoUpdateTimer_Elapsed);
+            
+            using (StreamWriter wr = new StreamWriter(path))
+            {
+                wr.WriteLine("Hi");
+            }
+            
+                
+            
+
+
+            // Setup a timer which prevents a VJoy popup message.
+
+            // joyResetTimer.Elapsed += new ElapsedEventHandler(joyResetTimer_Elapsed);
+        }
+
+        public bool DoesExist()
+        {
+            return File.Exists(path) ? true : false;
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Find all connected Wii devices.
+
+                var deviceCollection = new WiimoteCollection();
+                deviceCollection.FindAllWiimotes();
+
+                for (int i = 0; i < deviceCollection.Count; i++)
+                {
+                    wiiDevice = deviceCollection[i];
+
+                    // Device type can only be found after connection, so prompt for multiple devices.
+
+                    if (deviceCollection.Count > 1)
+                    {
+                        var devicePathId = new Regex("e_pid&.*?&(.*?)&").Match(wiiDevice.HIDDevicePath).Groups[1].Value.ToUpper();
+
+                        var response = MessageBox.Show("Connect to HID " + devicePathId + " device " + (i + 1) + " of " + deviceCollection.Count + " ?", "Multiple Wii Devices Found", MessageBoxButtons.YesNoCancel);
+                        if (response == DialogResult.Cancel) return;
+                        if (response == DialogResult.No) continue;
+                    }
+
+                    // Setup update handlers.
+
+                    wiiDevice.WiimoteChanged += wiiDevice_WiimoteChanged;
+                    wiiDevice.WiimoteExtensionChanged += wiiDevice_WiimoteExtensionChanged;
+
+                    // Connect and send a request to verify it worked.
+
+                    wiiDevice.Connect();
+                    wiiDevice.SetReportType(InputReport.IRAccel, false); // FALSE = DEVICE ONLY SENDS UPDATES WHEN VALUES CHANGE!
+                    wiiDevice.SetLEDs(true, false, false, false);
+
+                    // Enable processing of updates.
+
+                    infoUpdateTimer.Enabled = true;
+
+                    // Prevent connect being pressed more than once.
+
+                    button_Connect.Enabled = false;
+                    //button_BluetoothAddDevice.Enabled = false;
+                    break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void wiiDevice_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            // Called every time there is a sensor update, values available using e.WiimoteState.
+            // Use this for tracking and filtering rapid accelerometer and gyroscope sensor data.
+            // The balance board values are basic, so can be accessed directly only when needed.
+        }
+
+        private void wiiDevice_WiimoteExtensionChanged(object sender, WiimoteExtensionChangedEventArgs e)
+        {
+            // This is not needed for balance boards.
+        }
+
+        void infoUpdateTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Pass event onto the form GUI thread.
+
+            this.BeginInvoke(new Action(() => InfoUpdate()));
+        }
+
+        private void InfoUpdate()
+        {
+            if (wiiDevice.WiimoteState.ExtensionType != ExtensionType.BalanceBoard)
+            {
+                //label_Status.Text = "DEVICE IS NOT A BALANCE BOARD...";
+                return;
+            }
+
+            // Get the current raw sensor KG values.
+
+            var rwWeight = wiiDevice.WiimoteState.BalanceBoardState.WeightKg;
+
+            var rwTopLeft = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesKg.TopLeft;
+            var rwTopRight = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesKg.TopRight;
+            var rwBottomLeft = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesKg.BottomLeft;
+            var rwBottomRight = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesKg.BottomRight;
+
+            // The alternative .SensorValuesRaw is not adjusted with 17KG and 34KG calibration data, but does that make for better or worse control?
+            //
+            //var rwTopLeft     = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesRaw.TopLeft     - wiiDevice.WiimoteState.BalanceBoardState.CalibrationInfo.Kg0.TopLeft;
+            //var rwTopRight    = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesRaw.TopRight    - wiiDevice.WiimoteState.BalanceBoardState.CalibrationInfo.Kg0.TopRight;
+            //var rwBottomLeft  = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesRaw.BottomLeft  - wiiDevice.WiimoteState.BalanceBoardState.CalibrationInfo.Kg0.BottomLeft;
+            //var rwBottomRight = wiiDevice.WiimoteState.BalanceBoardState.SensorValuesRaw.BottomRight - wiiDevice.WiimoteState.BalanceBoardState.CalibrationInfo.Kg0.BottomRight;
+
+            // Show the raw sensor values.
+
+            label_rwWT.Text = rwWeight.ToString("0.0");
+            label_rwTL.Text = rwTopLeft.ToString("0.0");
+            label_rwTR.Text = rwTopRight.ToString("0.0");
+            label_rwBL.Text = rwBottomLeft.ToString("0.0");
+            label_rwBR.Text = rwBottomRight.ToString("0.0");
+
+            try
+            {
+                using (StreamWriter wr = new StreamWriter(path))
+                {
+
+
+                    wr.WriteLine("TL = " + label_rwTL.Text);
+                    wr.WriteLine("TR = " + label_rwTR.Text);
+                    wr.WriteLine("BL = " + label_rwBL.Text);
+                    wr.WriteLine("BR = " + label_rwBR.Text);
+
+                }
+            }
+            catch (IOException e)
+            {
+                
+            }
+
+
+            // Update joystick emulator.
+
+            //if (checkBox_EnableJoystick.Checked)
+            //{
+            //    // Uses Int16 ( -32767 to +32767 ) where 0 is the center. Multiplied by 2 because realistic usage is between the 30-70% ratio.
+
+            //    var joyX = (brX * 655.34 + -32767.0) * 2.0;
+            //    var joyY = (brY * 655.34 + -32767.0) * 2.0;
+
+            //    // Limit values to Int16, you cannot just (cast) or Convert.ToIn16() as the value '+ - sign' may invert.
+
+            //    if (joyX < short.MinValue) joyX = short.MinValue;
+            //    if (joyY < short.MinValue) joyY = short.MinValue;
+
+            //    if (joyX > short.MaxValue) joyX = short.MaxValue;
+            //    if (joyY > short.MaxValue) joyY = short.MaxValue;
+
+            //    // Set new values.
+
+            //    joyDevice.SetXAxis(0, (short)joyX);
+            //    joyDevice.SetYAxis(0, (short)joyY);
+            //    joyDevice.Update(0);
+            //}
+        }
+    }
+}
